12d927abc992a2bd1746983fb169f9d8ad96404a
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-17 11:57:02 +0300

    feat: add permission, role, and user list handlers with event listeners and modal integration

---
+++ laravel-permission-challenge/resources/js/permission/init.js
@@ -0,0 +1,30 @@
+import{
+    PermissionListHandlerListHandler,
+    RoleListHandler,
+    UserListHandler
+
+}from './permission';
+
+
+if (document.getElementById('permission-search-form')) {
+    new PermissionListHandler({
+        indexRoute: AppData.PermissionsIndexRoute,
+        csrfToken: AppData.csrfToken,
+    });
+}
+if (document.getElementById('role-search-form')) {
+    new RoleListHandler({
+        indexRoute:AppData.RolesIndexRoute,
+        csrfToken: AppData.csrfToken,
+
+    });
+}
+if (document.getElementById('user-search-form')) {
+    new UserListHandler({
+        indexRoute:AppData.UsersIndexRoute,
+        csrfToken: AppData.csrfToken,
+
+    });
+}
+
+
---
+++ laravel-permission-challenge/resources/js/permission/permission.js
@@ -0,0 +1,89 @@
+import ListHandler from "../../base/ListHandler";
+
+
+
+export class PermissionListHandler extends ListHandler {
+    constructor(options) {
+        super({
+            ...options,
+            entityName: 'permission',
+            routeName: 'permissions',
+            modalAddFormId: 'permissionCreateModal',
+            modalEditFormId: 'permissionEditModal',
+            modalViewFormId: 'permissionViewModal',
+        });
+    }
+
+    initEmployeeSelect2() {
+        // Use the generic function for this modal
+        const $select = $('.select2-ajax[name="user_id"]');
+        initEmployeeSelect2($select);
+    }
+
+    setupEventListeners() {
+        super.setupEventListeners();
+        const namespace = `.${this.entityName}Handler`;
+
+        // Re-initialize Select2 when modal is shown (create or edit)
+        $(document).on('shown.bs.modal', `#${this.modalAddFormId}, #${this.modalEditFormId}`, () => {
+            this.initEmployeeSelect2();
+        });
+    } 
+}
+
+export class RoleListHandler extends ListHandler {
+    constructor(options) {
+        super({
+            ...options,
+            entityName: 'role',
+            routeName: 'roles',
+            modalAddFormId: 'roleCreateModal',
+            modalEditFormId: 'roleEditModal',
+            modalViewFormId: 'roleViewModal',
+        });
+    }
+
+    initEmployeeSelect2() {
+        // Use the generic function for this modal
+        const $select = $('.select2-ajax[name="user_id"]');
+        initEmployeeSelect2($select);
+    }
+
+    setupEventListeners() {
+        super.setupEventListeners();
+        const namespace = `.${this.entityName}Handler`;
+
+        // Re-initialize Select2 when modal is shown (create or edit)
+        $(document).on('shown.bs.modal', `#${this.modalAddFormId}, #${this.modalEditFormId}`, () => {
+            this.initEmployeeSelect2();
+        });
+    }
+}
+export class UserListHandler extends ListHandler {
+    constructor(options) {
+        super({
+            ...options,
+            entityName: 'user',
+            routeName: 'users',
+            modalAddFormId: 'userCreateModal',
+            modalEditFormId: 'userEditModal',
+            modalViewFormId: 'userViewModal',
+        });
+    }
+
+    initEmployeeSelect2() {
+        // Use the generic function for this modal
+        const $select = $('.select2-ajax[name="user_id"]');
+        initEmployeeSelect2($select);
+    }
+
+    setupEventListeners() {
+        super.setupEventListeners();
+        const namespace = `.${this.entityName}Handler`;
+
+        // Re-initialize Select2 when modal is shown (create or edit)
+        $(document).on('shown.bs.modal', `#${this.modalAddFormId}, #${this.modalEditFormId}`, () => {
+            this.initEmployeeSelect2();
+        });
+    }
+}
