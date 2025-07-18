2ec7524e8c44272399fb0ca7a7ddf9866c1030f1
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-18 04:30:27 +0300

    feat: create articles table with user association and approval status

--- laravel-permission-challenge/database/migrations/2025_07_17_064537_create_articles_table.php
+++ laravel-permission-challenge/database/migrations/2025_07_17_064537_create_articles_table.php
@@ -10,12 +10,17 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('articles', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('title');
+        $table->text('content');
+        $table->boolean('approved')->default(false);
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
