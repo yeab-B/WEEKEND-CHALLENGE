84291637bc9c7a2132b81f432c9f1bbde25451bb
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-18 09:07:14 +0300

    fix: update article view paths to include user namespace

--- laravel-permission-challenge/app/Http/Controllers/ArticleController.php
+++ laravel-permission-challenge/app/Http/Controllers/ArticleController.php
@@ -22,12 +22,12 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::latest()->paginate(10);
-        return view('articles.index', compact('articles'));
+        return view('user.articles.index', compact('articles'));
     }
 
     public function create()
     {
-        return view('articles.create');
+        return view('user.articles.create');
     }
 
     public function store(Request $request)
@@ -48,12 +48,12 @@ class ArticleController extends Controller
 
     public function show(Article $article)
     {
-        return view('articles.show', compact('article'));
+        return view('user.articles.show', compact('article'));
     }
 
     public function edit(Article $article)
     {
-        return view('articles.edit', compact('article'));
+        return view('user.articles.edit', compact('article'));
     }
 
     public function update(Request $request, Article $article)
@@ -71,7 +71,7 @@ class ArticleController extends Controller
     public function destroy(Article $article)
     {
         $article->delete();
-        return redirect()->route('articles.index')->with('success', 'Article deleted.');
+        return redirect()->route('user.articles.index')->with('success', 'Article deleted.');
     }
 
     public function approve(Article $article)
@@ -79,6 +79,6 @@ class ArticleController extends Controller
         $article->approved = true;
         $article->save();
 
-        return redirect()->route('articles.index')->with('success', 'Article approved.');
+        return redirect()->route('user.articles.index')->with('success', 'Article approved.');
     }
 }
