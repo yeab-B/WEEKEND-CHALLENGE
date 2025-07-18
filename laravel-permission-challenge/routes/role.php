1d8768780faf7cee4cf7f029b406f9ed56569e10
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-18 04:11:33 +0300

    refactor: simplify role routes by removing unnecessary prefix grouping

--- laravel-permission-challenge/routes/role.php
+++ laravel-permission-challenge/routes/role.php
@@ -1,34 +1,21 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\UserController;
 
 
-// Role and Permission Routes
-Route::prefix('roles')->group(function () {
-    Route::get('/', [RoleController::class, 'index'])->name('roles.index');
-
-   
-        Route::get('/create', [RoleController::class, 'create'])->name('roles.create');
-        Route::post('/', [RoleController::class, 'store'])->name('roles.store');
- 
-
-  
-        Route::get('/{id}/edit', [RoleController::class, 'edit'])->name('roles.edit');
-        Route::put('/{role}', [RoleController::class, 'update'])->name('roles.update');
- 
-
-        Route::delete('/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
-   
-});
-// Permission Routes
+Route::get('roles/', [RoleController::class, 'index'])->name('roles.index');
+Route::get('roles/create', [RoleController::class, 'create'])->name('roles.create');
+Route::post('roles', [RoleController::class, 'store'])->name('roles.store');
+Route::get('roles/{id}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+Route::put('roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+Route::delete('roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 Route::resource('permissions', PermissionController::class)->except(['create', 'show']);
-Route::get('permissions', [PermissionController::class, 'index'])->name('permissions.index');
 Route::get('permissions/{id}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
 Route::put('permissions/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
 Route::delete('permissions/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
-
 
 // User Routes
 // Route::resource('users', UserController::class);
