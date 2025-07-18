bbdac01e469252e2fb82659d914b74f23e1d8e1a
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-18 04:38:03 +0300

    feat: add article routes for CRUD operations and approval

---
+++ laravel-permission-challenge/routes/artice.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/articles', [ArticleController::class, 'index'])
+    ->name('articles.index');
+
+Route::get('/articles/create', [ArticleController::class, 'create'])
+    ->name('articles.create');
+    
+
+Route::post('/articles', [ArticleController::class, 'store'])
+    ->name('articles.store');
+
+
+Route::get('/articles/{article}', [ArticleController::class, 'show'])
+    ->name('articles.show');
+
+Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])
+    ->name('articles.edit');
+
+Route::put('/articles/{article}', [ArticleController::class, 'update'])
+    ->name('articles.update');
+
+Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])
+    ->name('articles.destroy');
+
+Route::post('/articles/{article}/approve', [ArticleController::class, 'approve'])
+    ->name('articles.approve');
