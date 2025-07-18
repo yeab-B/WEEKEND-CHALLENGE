6d2eacf5ae7d3e77cb5227e29cee8f784856d0df
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-18 04:10:32 +0300

    fix: update Bootstrap CSS and JS links to version 5.3.3

--- laravel-permission-challenge/resources/views/layouts/app.blade.php
+++ laravel-permission-challenge/resources/views/layouts/app.blade.php
@@ -14,7 +14,10 @@
 <!-- Bootstrap JS Bundle with Popper (before </body>) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
 
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
