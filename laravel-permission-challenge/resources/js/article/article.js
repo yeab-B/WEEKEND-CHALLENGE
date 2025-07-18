d6bca5c3bcf595d323b399bf1f3e5ee72e780c90
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-18 04:49:28 +0300

    feat: implement ArticleListHandler for managing article-related modals and events

---
+++ laravel-permission-challenge/resources/js/article/article.js
@@ -0,0 +1,32 @@
+import ListHandler from "../base/ListHandler";
+
+
+
+export class ArticleListHandler extends ListHandler {
+    constructor(options) {
+        super({
+            ...options,
+            entityName: 'article',
+            routeName: 'articles',
+            modalAddFormId: 'articleCreateModal',
+            modalEditFormId: 'articleEditModal',
+            modalViewFormId: 'articleViewModal',
+        });
+    }
+
+    initEmployeeSelect2() {
+        // Use the generic function for this modal
+        const $select = $('.select2-ajax[name="user_id"]');
+        initEmployeeSelect2($select);
+    }
+
+    setupEventListeners() {
+        super.setupEventListeners();
+        const namespace = `.${this.entityName}Handler`;
+
+        // Re-initialize Select2 when modal is shown (create or edit)
+        $(document).on('shown.bs.modal', `#${this.modalAddFormId}, #${this.modalEditFormId}`, () => {
+            this.initEmployeeSelect2();
+        });
+    } 
+}
