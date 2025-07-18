0f495b84ae62c3de435f62ad0587f0ee4971d8f5
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-18 04:42:42 +0300

    refactor: remove UserController and related views for user management

--- laravel-permission-challenge/routes/role.php
+++ laravel-permission-challenge/routes/role.php
@@ -16,9 +16,3 @@ Route::resource('permissions', PermissionController::class)->except(['create', '
 Route::get('permissions/{id}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
 Route::put('permissions/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
 Route::delete('permissions/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
-
-// User Routes
-// Route::resource('users', UserController::class);
-// Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-// Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
-// Route::post('users/{id}/archived', [UserController::class, 'archive'])->name('users.archive');
