4031783e87ea05142251f75bcff6ed8bee2b5133
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-19 01:19:12 +0300

    fix: update role routes to use consistent parameter naming and method types

--- laravel-permission-challenge/routes/role.php
+++ laravel-permission-challenge/routes/role.php
@@ -9,10 +9,13 @@ use App\Http\Controllers\UserController;
 Route::get('roles/', [RoleController::class, 'index'])->name('roles.index');
 Route::get('roles/create', [RoleController::class, 'create'])->name('roles.create');
 Route::post('roles', [RoleController::class, 'store'])->name('roles.store');
-Route::get('roles/{id}/edit', [RoleController::class, 'edit'])->name('roles.edit');
-Route::put('roles/{role}', [RoleController::class, 'update'])->name('roles.update');
-Route::delete('roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
+Route::get('roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+// Route::put('roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+Route::match(['PUT', 'PATCH'], '/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+
+// Route::delete('roles/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
+Route::patch('roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 Route::resource('permissions', PermissionController::class)->except(['create', 'show']);
 Route::get('permissions/{id}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
 Route::put('permissions/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
-Route::delete('permissions/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+Route::patch('permissions/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
