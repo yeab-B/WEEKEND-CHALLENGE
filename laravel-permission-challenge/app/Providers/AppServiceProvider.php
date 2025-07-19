39aa83cdd02852b90bdaf4b66ae655f930cafd2f
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-19 15:33:32 +0300

    feat: register MenuComposer for sidebar view in AppServiceProvider

--- laravel-permission-challenge/app/Providers/AppServiceProvider.php
+++ laravel-permission-challenge/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use App\View\Composers\MenuComposer;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //  View::composer('layouts.sidebar', MenuComposer::class);
     }
 }
