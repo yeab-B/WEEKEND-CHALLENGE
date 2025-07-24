196b2e4de9fd7218830fa61426a986961c76b1f4
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-24 14:05:16 +0300

    refactor: rename AppServiceProvider to RouteServiceProvider and update routing logic

--- movie-backend/app/Providers/AppServiceProvider.php
+++ movie-backend/app/Providers/AppServiceProvider.php
@@ -2,23 +2,27 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
-class AppServiceProvider extends ServiceProvider
+class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * The path to your application's "home" route.
      */
-    public function register(): void
-    {
-        //
-    }
+    public const HOME = '/home';
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->routes(function () {
+           
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+       
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+        });
     }
 }
