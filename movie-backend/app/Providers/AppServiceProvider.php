1cdb7a60e972b49091ddac8c8ad8cf8ee3879470
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-26 16:36:06 +0300

    feat: import AuthServiceProvider in RouteServiceProvider for future authentication enhancements

--- movie-backend/app/Providers/AppServiceProvider.php
+++ movie-backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as AuthServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
