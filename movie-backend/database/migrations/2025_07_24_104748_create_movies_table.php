494ba97c88de86dbe073129412c23b2355753748
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-26 16:36:31 +0300

    feat: create movies table with necessary fields and relationships

--- movie-backend/database/migrations/2025_07_24_104748_create_movies_table.php
+++ movie-backend/database/migrations/2025_07_24_104748_create_movies_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+   
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumbnail_url')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
