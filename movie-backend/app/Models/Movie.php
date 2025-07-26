fb883f9e0d8007e04e493c9595af12da3d836a75
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-26 16:35:48 +0300

    feat: define fillable attributes and relationships in Movie model

--- movie-backend/app/Models/Movie.php
+++ movie-backend/app/Models/Movie.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    protected $fillable = [
+    'title',
+    'description',
+    'thumbnail_url',
+    'user_id',
+];
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
