c2d3577ceb47041874f5953562a72237d741ba80
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-25 12:44:50 +0300

    feat: add LanguageSeeder to DatabaseSeeder for language data population

--- laravel-permission-challenge/database/seeders/DatabaseSeeder.php
+++ laravel-permission-challenge/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
        $this->call([
            PermissionSeeder::class,
            UserSeeder::class,
+              LanguageSeeder::class,
         ]);
     }
 }
